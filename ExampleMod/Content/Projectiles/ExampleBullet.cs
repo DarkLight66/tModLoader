7d2d7ea87dd870160b1a3592fde51b243dcbcbb8
DarkLight66 <wence_409@yahoo.com>
2021-04-20 21:47:20 -0400

    Reimplemented `PreDrawExtras`, and reworked `PreDraw` and `PostDraw` projectile hooks
    * Projectile drawing hooks no longer receive the spritebatch as a parameter, as vanilla now uses `Main.EntitySpriteDraw` in a similar manner to draw projectile sprites, which takes care of applying shaders from dyes.
    * Changed documentation on the drawing hooks to mention the use of  `Main.EntitySpriteDraw` for drawing.
    * Changed vanilla code patches in such a way so that now `PreDrawExtras` can also stop yoyo strings from drawing, `PreDraw` can no stop all other vanilla drawing from happening, and `PostDraw` now properly runs after all vanilla projectile drawing.
    * Changed `lightColor` parameter in `PreDraw` so it's passed by reference.
    * Fixed `ExampleHook` chain drawing code so there isn't big gaps between each chain link.

--- ExampleMod/Content/Projectiles/ExampleBullet.cs
+++ ExampleMod/Content/Projectiles/ExampleBullet.cs
@@ -67,7 +67,7 @@ namespace ExampleMod.Content.Projectiles
 			}
 		}
 
-		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor) {
+		public override bool PreDraw(ref Color lightColor) {
 			Main.instance.LoadProjectile(Projectile.type);
 			Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
 
@@ -76,7 +76,7 @@ namespace ExampleMod.Content.Projectiles
 			for (int k = 0; k < Projectile.oldPos.Length; k++) {
 				Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
 				Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
-				spriteBatch.Draw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0f);
+				Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
 			}
 
 			return true;
